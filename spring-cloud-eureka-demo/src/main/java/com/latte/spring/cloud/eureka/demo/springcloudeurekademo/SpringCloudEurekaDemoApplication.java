1eb5d46caf8c5d63895c1f2582814e3a20cffa39
Latte <codsoul@163.com>
2018-01-22 23:25:35 +0800

    #refs -> Demo SpringBoot Eureka

---
+++ spring-cloud-eureka-demo/src/main/java/com/latte/spring/cloud/eureka/demo/springcloudeurekademo/SpringCloudEurekaDemoApplication.java
@@ -0,0 +1,14 @@
+package com.latte.spring.cloud.eureka.demo.springcloudeurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudEurekaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaDemoApplication.class, args);
+	}
+}
