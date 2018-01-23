8b231926ef900b48f7add1cf4227fd02e95e18d4
Latte <codsoul@163.com>
2018-01-23 23:01:22 +0800

    #refs -> Demo SpringCloud Ribbon

---
+++ spring-cloud-service-ribbon-demo/src/main/java/com/latte/spring/cloud/eureka/demo/springcloudeurekademo/HomeService.java
@@ -0,0 +1,16 @@
+package com.latte.spring.cloud.eureka.demo.springcloudeurekademo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HomeService {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public String hiService(String name) {
+		return restTemplate.getForObject("http://EUREKA-DEMO-CLIENT/home?userName=" + name, String.class);
+	}
+}
